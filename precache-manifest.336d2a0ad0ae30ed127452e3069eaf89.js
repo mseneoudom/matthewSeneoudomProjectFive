e217b807b3d8002400a31d5ef600db5d062107f8
mseneoudom <matt_sen@live.ca>
2020-05-03 20:58:59 -0400

    Updates

---
+++ precache-manifest.336d2a0ad0ae30ed127452e3069eaf89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31e3cb7aae6278cb7d85d47d230c3978",
+    "url": "/matthewSeneoudomProjectFive/index.html"
+  },
+  {
+    "revision": "6e9015a644877458ab0c",
+    "url": "/matthewSeneoudomProjectFive/static/css/main.75d9b649.chunk.css"
+  },
+  {
+    "revision": "f896e8e096bb773e5f3d",
+    "url": "/matthewSeneoudomProjectFive/static/js/2.8f2525c8.chunk.js"
+  },
+  {
+    "revision": "78aa0676509153305bde072ed1e9cb1c",
+    "url": "/matthewSeneoudomProjectFive/static/js/2.8f2525c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e9015a644877458ab0c",
+    "url": "/matthewSeneoudomProjectFive/static/js/main.420abe12.chunk.js"
+  },
+  {
+    "revision": "62ce5b0c377f90179c68",
+    "url": "/matthewSeneoudomProjectFive/static/js/runtime-main.da5fe469.js"
+  }
+]);
